88e8a696f111f741fb16bb07fcdf0fc0c1ea85ec
Santiago Mosquera Mápura <santiagomosquera2@gmail.com>
2025-05-30 21:42:30 +0000

    Redesign Java Editor Page and create an editor that can be wider and more user friendly. User should be able to modify size of editor

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import React from 'react';
-import JavaEditor from '@/components/JavaEditor'; // Import the new reusable component
+import JavaEditor from '@/components/JavaEditor';
 import { Loader2 } from 'lucide-react';
 
 export default function JavaEditorPage() {
@@ -21,10 +21,15 @@ export default function JavaEditorPage() {
   }
 
   return (
-    <div className="space-y-8 h-full flex flex-col">
-      <h1 className="text-3xl font-bold font-headline">Java Online Compiler</h1>
-      <div className="flex-grow">
-        <JavaEditor localStorageSuffix="_standalone" /> 
+    <div className="flex flex-col h-full"> {/* Occupy full height of AppShell's main content area */}
+      <div className="py-4"> {/* Compact header for the title */}
+        <h1 className="text-3xl font-bold font-headline text-center sm:text-left">
+          Java Online Compiler
+        </h1>
+      </div>
+      <div className="flex-grow flex flex-col min-h-0"> {/* Allows JavaEditor to expand */}
+        {/* JavaEditor component is designed to fill its container */}
+        <JavaEditor localStorageSuffix="_standalone_editor_page" />
       </div>
     </div>
   );
